59b952b25413bac15fa693570b582273439dc38b
aleksander_sidorenko <aleksander.sidorenko@ormdigital.com>
2019-08-21 00:23:42 +0300

    LEAN-69

--- src/main/java/sds/chemicalexport/config/AppConfiguration.java
+++ src/main/java/sds/chemicalexport/config/AppConfiguration.java
@@ -1,5 +1,6 @@
 package sds.chemicalexport.config;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.Mongo;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -23,6 +24,7 @@ import com.npspot.jtransitlight.transport.JTransitLightTransportException;
 import sds.chemicalexport.domain.commands.ExportFile;
 import com.sds.storage.BlobStorage;
 import com.sds.storage.gridfs.GridFSBlobStorage;
+import org.apache.commons.lang.StringUtils;
 
 
 @Configuration
@@ -34,14 +36,22 @@ public class AppConfiguration  {
 
     @Bean
     BlobStorage getBlobStorage(
-            @Value("${spring.data.mongodb.uri}") String mongoConnectionString, 
-            @Value("${spring.data.mongodb.database-name}") String dbName) {
+            @Value("${spring.data.mongodb.uri}") String mongoConnectionString) {
         
-        LOGGER.info("Connecting to MongoDB using url {}", mongoConnectionString);
-        LOGGER.info("MongoDB database name: {}", dbName);
-        
+        ConnectionString cs = new ConnectionString(mongoConnectionString);
+        if(cs.getPassword() != null)
+        {
+            String secureConnectionString = cs.toString().replaceFirst(":" + String.valueOf(cs.getPassword()) + "@", ":" + StringUtils.repeat("*", cs.getPassword().length) + "@")
+                    .replaceFirst("//" + cs.getUsername() + ":", "//" + StringUtils.repeat("*", cs.getUsername().length()) + ":");
+            LOGGER.info("Connecting to MongoDB using url {}", secureConnectionString); 
+        }
+        else{
+            LOGGER.info("Connecting to MongoDB using url {}", mongoConnectionString);
+        }
+       
+        LOGGER.info("MongoDB database name: {}", cs.getDatabase());
         return new GridFSBlobStorage(new MongoClient(
-                new MongoClientURI(mongoConnectionString)).getDatabase(dbName));    
+                new MongoClientURI(mongoConnectionString)).getDatabase(cs.getDatabase()));    
     }
     
     @Bean
